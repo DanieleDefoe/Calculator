6f6bc13a002b06d9071146fdd2004eb3ef2c1bbb
Abuzar <abuzar28102004@gmail.com>
2023-02-28 11:21:22 +0300

    feat: enable input field, add Delete and C keys

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const clear = document.querySelector('.clear');
 const del = document.querySelector('.delete');
 
 const printChar = (e) => {
+    if (e.key === 'c') input.value = '';
+    if (e.key === 'Delete') input.value = input.value.slice(0, input.value.length - 1);
     e.target.blur();
+    e.stopPropagation();
     let character = e.target.getAttribute('accesskey');
     if (!character) {
         character = e.key;
     }
-    if (!/\d|[*\-+=\/]|Enter/.test(character)) return;
+    if (!/\d|[*\-+=\/\.]|Enter/.test(character)) return;
     if (input.value === '0' && character !== '.' && !/\W/.test(character)) input.value = '';
     if (character === '.' && input.value.includes('.')) return;
     if (input.value === '' && /\W/.test(character)) return;
     if (/\W/.test(character) && /\W/.test(input.value[input.value.length - 1])) return;
-    if (/[*+\-\/]/.test(input.value[input.value.length - 1]) && (character === 'Enter' || character === '=')) return;
+    if (/[*+\-\/\.]/.test(input.value[input.value.length - 1]) && (character === 'Enter' || character === '=')) return;
     if (character === '=' || character === 'Enter') {
         if (input.value === '') return;
         input.value = eval(input.value);
@@ -37,4 +40,5 @@ const deleteChar = () => {
 
 clear.addEventListener('click', clearInput);
 del.addEventListener('click', deleteChar);
+input.addEventListener('keypress', printChar);
 window.addEventListener('keypress', printChar);
